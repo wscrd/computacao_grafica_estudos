9708e7e4964bfb687b87badf87ab83abf98db2ae
Wesley Scardua <wesscardua@gmail.com>
2017-02-23 15:19:44 -0300

    Update opengl_01.c
    fixed some misplaced break lines

--- opengl_01.c
+++ opengl_01.c
@@ -7,11 +7,11 @@ void Desenha(void) {
   glClear(GL_COLOR_BUFFER_BIT);
   glColor3f(1.0f, 0.0f, 0.0f);
   glBegin(GL_POINTS);
-  glVertex2i(100,150);
-  glVertex2i(100,100);
-  glColor3f(0.0f, 0.0f, 1.0f);
-  glVertex2i(150,100);
-  glVertex2i(150,150);               
+    glVertex2i(100,150);
+    glVertex2i(100,100);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex2i(150,100);
+    glVertex2i(150,150);               
   glEnd();
   glFlush();
 }
@@ -26,14 +26,12 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h) {
   glViewport(0, 0, w, h);
   // Inicializa o sistema de coordenadas
   glMatrixMode(GL_PROJECTION);
-  glLoadIdentit
-  y();
+  glLoadIdentity();
   // Estabelece a janela de seleção (left, right, bottom, top)
   if (w <= h) 
-  gluOrtho2D (0.0f, 250.0f, 0.0f, 250.0f*h/w);
+    gluOrtho2D (0.0f, 250.0f, 0.0f, 250.0f*h/w);
   else 
-  gluOrtho2D (0.
-  0f, 250.0f*w/h, 0.0f, 250.0f);
+    gluOrtho2D (0.0f, 250.0f*w/h, 0.0f, 250.0f);
 }
 // Programa Principal 
 int main(int argc, char **argv) {
